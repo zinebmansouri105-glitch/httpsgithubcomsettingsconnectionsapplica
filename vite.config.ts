0a5593d4b56d1a9e3b75c2ed7caca0d30cb3b530
zinebmansouri105-glitch <zinebmansouri105@gmail.com>
2026-06-15 13:56:24 +0100

    Initial commit with project files

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react({ babel: { plugins: [
+function __dualiteSourceLoc({ types: t }) {
+  return { visitor: { JSXOpeningElement(path, state) {
+    var fn = state.filename || '';
+    if (!fn || fn.includes('node_modules')) return;
+    var name = path.node.name;
+    var reactSpecials = ['Fragment', 'StrictMode', 'Suspense', 'Profiler'];
+    var isReactSpecial = (name.type === 'JSXIdentifier' && reactSpecials.indexOf(name.name) !== -1) ||
+      (name.type === 'JSXMemberExpression' && name.object && name.object.name === 'React' && name.property && reactSpecials.indexOf(name.property.name) !== -1) ||
+      (name.type === 'JSXMemberExpression' && name.property && (name.property.name === 'Provider' || name.property.name === 'Consumer'));
+    if (isReactSpecial) return;
+    var attrs = path.node.attributes;
+    for (var i = 0; i < attrs.length; i++) {
+      if (attrs[i].type === 'JSXAttribute' && attrs[i].name && attrs[i].name.name === 'data-ds') return;
+    }
+    var loc = path.node.loc;
+    if (!loc) return;
+    var wd = '/home/project/';
+    var rel = fn.startsWith(wd) ? fn.slice(wd.length) : fn;
+    attrs.push(t.jsxAttribute(t.jsxIdentifier('data-ds'), t.stringLiteral(rel + ':' + loc.start.line + ':' + loc.start.column)));
+  } } };
+}
+] } })],
+  
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
